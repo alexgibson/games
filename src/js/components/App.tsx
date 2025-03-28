4a39af5b9db830c8d8339aafec65d6b167b82cd4
Alex Gibson <alexgibson@users.noreply.github.com>
2025-03-28 15:13:50 +0000

    Use hook for details open / closed state

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,9 +1,15 @@
 import Details from "./Details";
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 import { Status } from "../types.ts";
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (e: React.SyntheticEvent<HTMLDetailsElement>) => {
+    setIsOpen(e.currentTarget.open);
+  };
+
   const allStatus: Status[] = [
     "Playing",
     "Backlog",
@@ -15,7 +21,12 @@ export default function App() {
   return (
     <>
       {allStatus.map((status: string) => (
-        <Details key={status} title={status} open={true}>
+        <Details
+          key={status}
+          title={status}
+          open={isOpen}
+          onClick={handleToggle}
+        >
           <Table status={status}></Table>
         </Details>
       ))}
