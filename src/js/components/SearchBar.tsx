74adde46884df4d81fed79695cabbf80096257fc
Alex Gibson <alexgibson@users.noreply.github.com>
2025-04-02 09:14:07 +0100

    Add clear button to search filter

--- src/js/components/SearchBar.tsx
+++ src/js/components/SearchBar.tsx
@@ -4,6 +4,7 @@ type SearchBarProps = React.HTMLAttributes<HTMLFormElement> & {
   placeholder: string;
   value: string;
   onSearch: React.FormEventHandler<HTMLFormElement>;
+  onClear: () => void;
   ariaControls: string;
 };
 
@@ -11,21 +12,30 @@ const SearchBar: React.FC<SearchBarProps> = ({
   placeholder,
   value,
   onSearch,
+  onClear,
   ariaControls,
 }) => {
+  const isDisabled = value ? false : true;
+
   return (
     <form onSubmit={onSearch}>
       <label htmlFor="search">Search</label>
       <input
+        key={value}
         id="search"
         type="search"
         name="search"
-        defaultValue={value ?? ""}
+        defaultValue={value}
         placeholder={placeholder}
         aria-controls={ariaControls}
       />
-      <button name="submit" type="submit">
-        Go
+      <button
+        name="clear"
+        type="button"
+        disabled={isDisabled}
+        onClick={onClear}
+      >
+        Clear
       </button>
     </form>
   );
