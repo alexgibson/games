d706633f45741164bc719145e41772fa4b6de378
Alex Gibson <alexgibson@users.noreply.github.com>
2025-04-11 15:34:38 +0100

    Ensure full immutability when using sortByKey() and filterByKey()

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,5 +1,6 @@
 /**
- * Sorts a given array of objects by key.
+ * Takes an array of objects and returns a new copy
+ * of the array, sorted ascending or descending by key.
  * @param array of objects to be sorted.
  * @param key to sort by.
  * @param ascending (defaults to true).
@@ -10,30 +11,33 @@ export function sortByKey<T, K extends keyof T>(
   key: K,
   ascending: boolean = true,
 ): T[] {
-  return [...array].sort((a, b) => {
-    const valueA = a[key];
-    const valueB = b[key];
+  return [...array]
+    .sort((a, b) => {
+      const valueA = a[key];
+      const valueB = b[key];
 
-    if (typeof valueA === "string" && typeof valueB === "string") {
-      return ascending
-        ? valueA.localeCompare(valueB)
-        : valueB.localeCompare(valueA);
-    }
+      if (typeof valueA === "string" && typeof valueB === "string") {
+        return ascending
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      }
 
-    if (typeof valueA === "number" && typeof valueB === "number") {
-      return ascending ? valueA - valueB : valueB - valueA;
-    }
+      if (typeof valueA === "number" && typeof valueB === "number") {
+        return ascending ? valueA - valueB : valueB - valueA;
+      }
 
-    return 0;
-  });
+      return 0;
+    })
+    .map((obj) => ({ ...obj }));
 }
 
 /**
- * Filters a given array of objects by key / value.
- * @param array of objects to be sorted.
+ * Takes an array of objects and returns a new copy
+ * of the array, filtered by key / value pair.
+ * @param array of objects to be filtered.
  * @param key to filter by.
  * @param value to filter by.
- * @returns filtered array of objects.
+ * @returns filtered array.
  */
 export function filterByKey<T, K extends keyof T>(
   array: T[],
@@ -44,16 +48,19 @@ export function filterByKey<T, K extends keyof T>(
     return array;
   }
 
-  return array.filter((item) => {
-    const val = value.toString().toLocaleLowerCase();
-    if (String(item[key]).toLowerCase().includes(val)) {
-      return item;
-    }
-  });
+  return array
+    .filter((item) => {
+      const val = value.toString().toLocaleLowerCase();
+      if (String(item[key]).toLowerCase().includes(val)) {
+        return item;
+      }
+    })
+    .map((obj) => ({ ...obj }));
 }
 
 /**
- * Returns a shortened date string.
+ * Takes a Date() object and returns a short date string,
+ * formatted by locale code.
  * @param date object to format.
  * @param locale string (defaults to en-GB).
  * @returns string format DD/MM/YYY.
