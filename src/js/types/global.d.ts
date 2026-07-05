0196da3ca0abdcc5c23a18c49c0118b3beae9ab3
Alex Gibson <alexgibson@users.noreply.github.com>
2026-07-05 18:45:32 +0100

    Linting fix

--- src/js/types/global.d.ts
+++ src/js/types/global.d.ts
@@ -2,20 +2,10 @@ declare global {
   type Platform = "Nintendo Switch" | "Steam";
   type OneToTen = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | null;
   type Status =
-    | "Beat"
-    | "Playing"
-    | "Paused"
-    | "Backlog"
-    | "Wishlist"
-    | "Paused";
+    "Beat" | "Playing" | "Paused" | "Backlog" | "Wishlist" | "Paused";
   type Medium = "Physical | Digital" | "Game Key Card";
   type FieldName =
-    | "Title"
-    | "Platform"
-    | "Developer"
-    | "Release Date"
-    | "Medium"
-    | "Score";
+    "Title" | "Platform" | "Developer" | "Release Date" | "Medium" | "Score";
   interface Game {
     title: string;
     platform: Platform;
